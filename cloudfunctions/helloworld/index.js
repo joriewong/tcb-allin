fff8314a56cc5595601cb8ef060f6ac2adba734d
Jared Wang <stillba@icloud.com>
2021-05-24 17:32:26 +0800

    feat: github repos cloud fn

--- cloudfunctions/helloworld/index.js
+++ cloudfunctions/helloworld/index.js
@@ -1,14 +1,34 @@
 const cloud = require("@cloudbase/node-sdk");
+const axios = require("axios");
+const API_BASE_URL = "https://api.github.com";
+const USERNAME = "joriewong";
+const TOKEN = "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";
 
 exports.main = async (event, context) => {
   const app = cloud.init({
     env: cloud.SYMBOL_CURRENT_ENV,
   });
 
-  // todo
-  // your code here
+  let data = [],
+    message = "ok";
+
+  try {
+    const { data: repos } = await axios.get(
+      `${API_BASE_URL}/users/${USERNAME}/repos`,
+      {
+        headers: {
+          Authorization: `token ${TOKEN}`,
+        },
+      }
+    );
+    data = repos
+  } catch (error) {
+    message = error;
+  }
+
   return {
     event,
-    message: 'hello world'
+    message,
+    data,
   };
 };
