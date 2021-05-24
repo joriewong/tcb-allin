02c5b7510e4e3463b8ea98c59be0bfd7fe9993e6
Jared Wang <stillba@icloud.com>
2021-05-24 18:28:06 +0800

    feat: add pro table

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Hello from "./components/Hello";
 import cloudbaseLogo from "./assets/cloudbase.png";
 import reactLogo from "./assets/logo.png";
 import "./App.css";
+import ProTable from "@ant-design/pro-table";
 
 function App() {
+  const columns = []
+
   return (
     <div className="App">
       <header className="App-header">
         <img className="logo" alt="CloudBase logo" src={cloudbaseLogo} />
         <img src={reactLogo} className="react-logo" alt="logo" />
       </header>
+      <main className="App-main">
+        <ProTable columns={columns} />
+      </main>
       <Hello />
     </div>
   );
